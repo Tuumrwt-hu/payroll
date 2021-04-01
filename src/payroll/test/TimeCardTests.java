c4a7fe51a8e2fdf8ed9327fd57a686856f6969e6
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 22:31:04 +0800

    Add test one time card to hourly employee

--- src/payroll/test/TimeCardTests.java
+++ src/payroll/test/TimeCardTests.java
@@ -4,11 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import payroll.Employee;
+import payroll.PaymentClassification;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.classification.HourlyClassification;
+import payroll.trans.AddHourlyEmployeeTransaction;
+
 class TimeCardTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testOneTimeCardToHourlyEmployee() {
+		int empId = 3001;
+		new AddHourlyEmployeeTransaction(empId, "Bill", "Home", 12.5).execute();
+		assertNotNull(PayrollDatabase.getEmployee(empId));
+
+		String date = "2018-03-14";
+		double hours = 7.5;
+		Transaction t = new TimeCardTransaction(empId, date, hours);
+		t.execute();
+
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNotNull(e);
+		assertEquals(empId, e.getEmpId());
+		PaymentClassification pc = e.getPaymentClassification();
+		assertTrue(pc instanceof HourlyClassification);
+		HourlyClassification hc = (HourlyClassification) pc;
+		TimeCard tc = hc.getTimeCardOfDate(date);
+		assertNotNull(tc);
+		assertEquals(date, tc.getDate());
+		assertEquals(hours, tc.getHours(), 0.001);
 	}
 
 }
