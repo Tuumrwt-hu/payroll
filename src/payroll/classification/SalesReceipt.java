69445df785aea359a89205af625fe7d7f4f6a786
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 23:16:03 +0800

    Add getters in SalesReceipt

--- src/payroll/classification/SalesReceipt.java
+++ src/payroll/classification/SalesReceipt.java
@@ -2,4 +2,14 @@ package payroll.classification;
 
 public class SalesReceipt {
 
+	public Object getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
