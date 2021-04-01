f6da7e6d8446a1fc7809400a7e0d54f49beaac54
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:37:20 +0800

    Add test for add hourly employee

--- src/payroll/test/AddEmployeeTests.java
+++ src/payroll/test/AddEmployeeTests.java
@@ -6,9 +6,31 @@ import org.junit.jupiter.api.Test;
 
 class AddEmployeeTests {
 
+	// Test for
+	// AddEmp empId name address H hourly-rate
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAddHourlyEmployee() {
+		// Assumptions
+		int empId = 1001;
+		String name = "Bill";
+		String address = "Home";
+		double hourlyRate = 12.5;
+		// Actions
+		Transaction t = new AddHourlyEmployeeTransaction(
+				empId, name, address, hourlyRate);
+		t.execute();
+		// Assertions
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNotNull(e);
+		assertEquals(empId, e.getEmpId());
+		assertEquals(name, e.getName());
+		assertEquals(address, e.getAddress());
+		PaymentClassification pc = e.getPaymentClassification();
+		assertTrue(pc instanceof HourlyClassification);
+		HourlyClassification hc = (HourlyClassification) pc;
+		assertEquals(hourlyRate, hc.getHourlyRate(), 0.01);
+		PaymentMethod pm = e.getPaymentMethod();
+		assertTrue(pm instanceof HoldMethod);
 	}
 
 }
