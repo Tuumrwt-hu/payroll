2b759956ca50f2a4d45cb41c11f24cbcfd0be5d1
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 22:35:27 +0800

    Create constructor

--- src/payroll/trans/TimeCardTransaction.java
+++ src/payroll/trans/TimeCardTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class TimeCardTransaction implements Transaction {
 
+	public TimeCardTransaction(int empId, String date, double hours) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
