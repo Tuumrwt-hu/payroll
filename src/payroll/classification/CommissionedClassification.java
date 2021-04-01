c9e9fd95e27446b60fcecd81e2091f191b41f5b6
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 21:52:17 +0800

    Add constructor

--- src/payroll/classification/CommissionedClassification.java
+++ src/payroll/classification/CommissionedClassification.java
@@ -4,6 +4,10 @@ import payroll.PaymentClassification;
 
 public class CommissionedClassification extends PaymentClassification {
 
+	public CommissionedClassification(double salary, double commissionRate) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public double getSalary() {
 		// TODO Auto-generated method stub
 		return 0;
