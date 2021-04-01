9851a4ff5eff8604210e1d3981e44dbcfeab06c8
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:39:42 +0800

    Create class AddHourlyEmpTrans

--- src/payroll/test/AddEmployeeTests.java
+++ src/payroll/test/AddEmployeeTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import payroll.Transaction;
+import payroll.trans.AddHourlyEmployeeTransaction;
 
 class AddEmployeeTests {
 
