1e6345ac7612eb7141c70363ad05dba1f17318ed
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:40:09 +0800

    Add constructor

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
