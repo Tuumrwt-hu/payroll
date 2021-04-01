b25ad16021dbc154db2747a9c31e3f6292aa2bca
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 23:12:39 +0800

    Add test one sales receipt to commissioned employee

--- src/payroll/test/SalesReceiptTests.java
+++ src/payroll/test/SalesReceiptTests.java
@@ -4,11 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import payroll.Employee;
+import payroll.PaymentClassification;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.classification.CommissionedClassification;
+import payroll.trans.AddCommissionedEmployeeTransaction;
+
 class SalesReceiptTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testOneSalesReceiptToCommissionedEmployee() {
+		int empId = 4001;
+		new AddCommissionedEmployeeTransaction(empId, "Bill", "Home", 3456.0, 0.02).execute();
+		assertNotNull(PayrollDatabase.getEmployee(empId));
+		
+		String date = "2018-03-14";
+		double amount = 1000;
+		Transaction t = new SalesReceiptTransaction(empId, date, amount);
+		t.execute();
+		
+		Employee employee = PayrollDatabase.getEmployee(empId);
+		assertNotNull(employee);
+		PaymentClassification pc = employee.getPaymentClassification();
+		assertTrue(pc instanceof CommissionedClassification);
+		CommissionedClassification cc = (CommissionedClassification) pc;
+		SalesReceipt sr = cc.getSalesReceiptOfDate(date);
+		assertEquals(date, sr.getDate());
+		assertEquals(amount, sr.getAmount(), 0.001);
 	}
 
 }
