f6bc93b7ba1a71bd2f56030ed346ea5d3477ff0c
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 23:13:55 +0800

    Create constructor

--- src/payroll/trans/SalesReceiptTransaction.java
+++ src/payroll/trans/SalesReceiptTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class SalesReceiptTransaction implements Transaction {
 
+	public SalesReceiptTransaction(int empId, String date, double amount) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
