af90c9085e63e1d20b688accf278470c3f8b7558
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 23:20:38 +0800

    Create constructor

--- src/payroll/classification/SalesReceipt.java
+++ src/payroll/classification/SalesReceipt.java
@@ -2,6 +2,10 @@ package payroll.classification;
 
 public class SalesReceipt {
 
+	public SalesReceipt(String date, double amount) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Object getDate() {
 		// TODO Auto-generated method stub
 		return null;
