a57427b60d3a3b57ce103529ccdd083ade4c13e8
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:51:51 +0800

    Assign all parameters to new fields

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,8 +4,16 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double hourlyRate;
+
 	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
-		// TODO Auto-generated constructor stub
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
 	}
 
 	@Override
