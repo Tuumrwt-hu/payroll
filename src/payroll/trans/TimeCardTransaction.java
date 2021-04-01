9c7d8a51cd56f39f2a2e374c918cd121c4eb2b4f
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 22:35:06 +0800

    Create class TimeCardTrans

---
+++ src/payroll/trans/TimeCardTransaction.java
@@ -0,0 +1,13 @@
+package payroll.trans;
+
+import payroll.Transaction;
+
+public class TimeCardTransaction implements Transaction {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
