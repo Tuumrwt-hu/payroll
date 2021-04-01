c2e51a978f37135358b61f3a1f0264e0ae3445e2
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 22:52:51 +0800

    Add test for two cards to hourly employee

--- src/payroll/test/TimeCardTests.java
+++ src/payroll/test/TimeCardTests.java
@@ -37,5 +37,35 @@ class TimeCardTests {
 		assertEquals(date, tc.getDate());
 		assertEquals(hours, tc.getHours(), 0.001);
 	}
+	
+	@Test
+	void testTwoTimeCardsToHourlyEmployee() {
+		int empId = 3002;
+		new AddHourlyEmployeeTransaction(empId, "Bill", "Home", 12.5).execute();
+		assertNotNull(PayrollDatabase.getEmployee(empId));
+
+		String date1 = "2018-03-14";
+		double hours1 = 5.5;
+		new TimeCardTransaction(empId, date1, hours1).execute();
+
+		String date2 = "2018-03-15";
+		double hours2 = 10.0;
+		new TimeCardTransaction(empId, date2, hours2).execute();
+
+		Employee employee = PayrollDatabase.getEmployee(empId);
+		PaymentClassification pc = employee.getPaymentClassification();
+		assertTrue(pc instanceof HourlyClassification);
+		HourlyClassification hc = (HourlyClassification) pc;
+
+		TimeCard timeCard1 = hc.getTimeCardOfDate(date1);
+		assertNotNull(timeCard1);
+		assertEquals(date1, timeCard1.getDate());
+		assertEquals(hours1, timeCard1.getHours(), 0.001);
+
+		TimeCard timeCard2 = hc.getTimeCardOfDate(date2);
+		assertNotNull(timeCard2);
+		assertEquals(date2, timeCard2.getDate());
+		assertEquals(hours2, timeCard2.getHours());
+	}
 
 }
