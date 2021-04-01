a858368e40f58e506708dd2d542afc0ffb1bf095
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:42:59 +0800

    Add getters for empId, name and address

--- src/payroll/Employee.java
+++ src/payroll/Employee.java
@@ -2,4 +2,19 @@ package payroll;
 
 public class Employee {
 
+	public Object getEmpId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
