606076e857d37037bc5de5e29ea9a863a28836db
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 22:37:27 +0800

    Add getters in TimeCard

--- src/payroll/classification/TimeCard.java
+++ src/payroll/classification/TimeCard.java
@@ -2,4 +2,14 @@ package payroll.classification;
 
 public class TimeCard {
 
+	public Object getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getHours() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
