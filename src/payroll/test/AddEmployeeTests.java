34492047cfc3af0c576b0c93a578224fb183e4d3
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-01 19:34:58 +0800

    Create test case AddEmployeeTests

---
+++ src/payroll/test/AddEmployeeTests.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddEmployeeTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
