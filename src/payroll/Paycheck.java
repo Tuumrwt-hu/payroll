0664a1a33bb4f8e926759fc2a63016533cb92543
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:28:35 +0800

    Add getters

--- src/payroll/Paycheck.java
+++ src/payroll/Paycheck.java
@@ -2,4 +2,24 @@ package payroll;
 
 public class Paycheck {
 
+	public Object getEmpId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getDisposition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
