75fae1f106515fba48d4555c171128912d609147
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:00:53 +0800

    Impl constructor

--- src/payroll/trans/ChangeCommissionedTransaction.java
+++ src/payroll/trans/ChangeCommissionedTransaction.java
@@ -5,8 +5,13 @@ import payroll.Transaction;
 
 public class ChangeCommissionedTransaction extends ChangeClassificationTransaction implements Transaction {
 
+	private double salary;
+	private double commissionRate;
+
 	public ChangeCommissionedTransaction(int empId, double salary, double commissionRate) {
-		// TODO Auto-generated constructor stub
+		super(empId);
+		this.salary = salary;
+		this.commissionRate = commissionRate;
 	}
 
 	@Override
