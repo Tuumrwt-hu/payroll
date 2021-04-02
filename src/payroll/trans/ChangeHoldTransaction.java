3c6b2e44637bfa43f40901981e0781f57f2dedf6
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:06:26 +0800

    Create constructor

--- src/payroll/trans/ChangeHoldTransaction.java
+++ src/payroll/trans/ChangeHoldTransaction.java
@@ -5,6 +5,10 @@ import payroll.Transaction;
 
 public class ChangeHoldTransaction extends ChangeEmployeeTransaction implements Transaction {
 
+	public ChangeHoldTransaction(int empId) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected void doChange(Employee e) {
 		// TODO Auto-generated method stub
