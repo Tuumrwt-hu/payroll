4f5d81694c78ec17502283ea5bd277e5a058b5b5
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:13:26 +0800

    Create constructor

--- src/payroll/method/DirectMethod.java
+++ src/payroll/method/DirectMethod.java
@@ -4,4 +4,8 @@ import payroll.PaymentMethod;
 
 public class DirectMethod extends PaymentMethod {
 
+	public DirectMethod(String bank, String account) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
