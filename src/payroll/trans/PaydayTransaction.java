43fb3f731c5b5582238f00fa3b1fad50db8cb376
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:51:50 +0800

    Impl pay for all employees and keep the paychecks

--- src/payroll/trans/PaydayTransaction.java
+++ src/payroll/trans/PaydayTransaction.java
@@ -1,19 +1,35 @@
 package payroll.trans;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import payroll.Employee;
 import payroll.Paycheck;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
 
 public class PaydayTransaction implements Transaction {
 
+	private Map<Integer, Paycheck> paychecks = new LinkedHashMap<>();
+	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		// Clear paychecks
+		paychecks.clear();
+		
+		// Pay for each employee and keep the paycheck
+		for (Employee e : PayrollDatabase.listAllEmployees()) {
+			// Create a blank paycheck
+			Paycheck paycheck = new Paycheck();
+			// Fill in the paycheck
+			e.pay(paycheck);
+			// Keep the paycheck
+			paychecks.put(e.getEmpId(), paycheck);
+		}
 	}
 
 	public Paycheck getPaycheckOf(int empId) {
-		// TODO Auto-generated method stub
-		return null;
+		return paychecks.get(empId);
 	}
 
 }
