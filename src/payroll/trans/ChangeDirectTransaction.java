4c4b96aeabe41bc7b2fdac18dae1a2ff04fc4467
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:10:48 +0800

    Create constructor

--- src/payroll/trans/ChangeDirectTransaction.java
+++ src/payroll/trans/ChangeDirectTransaction.java
@@ -5,6 +5,10 @@ import payroll.Transaction;
 
 public class ChangeDirectTransaction extends ChangeEmployeeTransaction implements Transaction {
 
+	public ChangeDirectTransaction(int empId, String bank, String account) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected void doChange(Employee e) {
 		// TODO Auto-generated method stub
