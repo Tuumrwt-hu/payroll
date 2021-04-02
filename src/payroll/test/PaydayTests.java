4f8cfb076a88a0b33cd560b573ef11bf3d7e46bd
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:21:58 +0800

    Add test case for payday

---
+++ src/payroll/test/PaydayTests.java
@@ -0,0 +1,16 @@
+package payroll.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+// Test for Payday
+
+class PaydayTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
