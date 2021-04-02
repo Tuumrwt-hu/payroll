4af82420129eb3ae66eb64dde606ac8b64888fd7
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 09:50:52 +0800

    Create constructor

--- src/payroll/trans/ChangeSalariedTransaction.java
+++ src/payroll/trans/ChangeSalariedTransaction.java
@@ -5,6 +5,10 @@ import payroll.Transaction;
 
 public class ChangeSalariedTransaction extends ChangeEmployeeTransaction implements Transaction {
 
+	public ChangeSalariedTransaction(int empId, double salary) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected void doChange(Employee e) {
 		// TODO Auto-generated method stub
