7065138992a7662015f1648cca23024dba0916f7
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:24:18 +0800

    Add test payday salaried hold

--- src/payroll/test/PaydayTests.java
+++ src/payroll/test/PaydayTests.java
@@ -1,16 +1,29 @@
 package payroll.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import payroll.trans.AddSalariedEmployeeTransaction;
 
 // Test for Payday
 
 class PaydayTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPaydaySalariedHold() {
+		int empId = 6001;
+		String name = "Bill";
+		double salary = 3000.0;
+		new AddSalariedEmployeeTransaction(empId, name, "Home", salary).execute();
+		PaydayTransaction t = new PaydayTransaction();
+		t.execute();
+
+		Paycheck p = t.getPaycheckOf(empId);
+		assertEquals(empId, p.getEmpId());
+		assertEquals(name, p.getName());
+		assertEquals(salary, p.getAmount(), 0.001);
+		assertEquals("HOLD", p.getDisposition());
 	}
 
 }
