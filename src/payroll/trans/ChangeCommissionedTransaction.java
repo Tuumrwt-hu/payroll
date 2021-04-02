0df8419e68d9e4c630a54ee728b85b2d1eb1d596
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:00:18 +0800

    Create constructor

--- src/payroll/trans/ChangeCommissionedTransaction.java
+++ src/payroll/trans/ChangeCommissionedTransaction.java
@@ -5,6 +5,10 @@ import payroll.Transaction;
 
 public class ChangeCommissionedTransaction extends ChangeClassificationTransaction implements Transaction {
 
+	public ChangeCommissionedTransaction(int empId, double salary, double commissionRate) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected PaymentClassification getPaymentClassification() {
 		// TODO Auto-generated method stub
