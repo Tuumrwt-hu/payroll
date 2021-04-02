b455cd7209517ef6f273ab70be3da668c603d87a
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 08:54:15 +0800

    Impl change name

--- src/payroll/trans/ChangeNameTransaction.java
+++ src/payroll/trans/ChangeNameTransaction.java
@@ -1,5 +1,7 @@
 package payroll.trans;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
 
 public class ChangeNameTransaction implements Transaction {
@@ -14,8 +16,10 @@ public class ChangeNameTransaction implements Transaction {
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		Employee e = PayrollDatabase.getEmployee(empId);
+		if (e != null) {
+			e.setName(newName);
+		}
 	}
 
 }
