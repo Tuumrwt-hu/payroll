e65061c2031e1a0e123478f8674a16a74c8a28b6
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 10:59:03 +0800

    Create constructor

--- src/payroll/trans/ChangeMailTransaction.java
+++ src/payroll/trans/ChangeMailTransaction.java
@@ -5,6 +5,10 @@ import payroll.Transaction;
 
 public class ChangeMailTransaction extends ChangeMethodTransaction implements Transaction {
 
+	public ChangeMailTransaction(int empId, String mailAddress) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected PaymentMethod getMethod() {
 		// TODO Auto-generated method stub
