a30eaafbcd6d9b9d0cdf03de96f67e56e12ecf95
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:27:38 +0800

    Add getPaycheckOf()

--- src/payroll/trans/PaydayTransaction.java
+++ src/payroll/trans/PaydayTransaction.java
@@ -1,5 +1,6 @@
 package payroll.trans;
 
+import payroll.Paycheck;
 import payroll.Transaction;
 
 public class PaydayTransaction implements Transaction {
@@ -8,6 +9,11 @@ public class PaydayTransaction implements Transaction {
 	public void execute() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Paycheck getPaycheckOf(int empId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
