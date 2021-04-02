20f03eae34e73d685caf99edcfc0a1860ecda146
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 09:30:26 +0800

    Create constructor

--- src/payroll/trans/ChangeAddressTransaction.java
+++ src/payroll/trans/ChangeAddressTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class ChangeAddressTransaction implements Transaction {
 
+	public ChangeAddressTransaction(int empId, String newAddress) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
