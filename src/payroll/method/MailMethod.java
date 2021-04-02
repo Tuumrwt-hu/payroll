ae648d6784095df0997503c5e04fd3dc51cd19c2
Tuumrwt-hu <dupajian1991da@126.com>
2021-04-02 11:02:20 +0800

    Create constructor

--- src/payroll/method/MailMethod.java
+++ src/payroll/method/MailMethod.java
@@ -4,6 +4,10 @@ import payroll.PaymentMethod;
 
 public class MailMethod extends PaymentMethod {
 
+	public MailMethod(String mailAddress) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Object getAddress() {
 		// TODO Auto-generated method stub
 		return null;
